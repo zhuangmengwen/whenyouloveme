f38f594b53de42f65760788e48a54381f8bfcc23
zhuangmengwen <zhuangmengwen@sunline.com>
2020-12-07 16:56:49 +0800

    git 提交

---
+++ TestJVM.java
@@ -0,0 +1,14 @@
+public class TestJVM {
+
+    public static void main(String[] args) {
+        // java -Djvm=Hello Main 设置环境变量
+        // Hello
+        String jvm = System.getProperty("jvm");
+        System.out.println(jvm);
+        if (jvm == null) {
+            System.out.println("test-jvm");
+        } else {
+            System.out.println(jvm);
+        }
+    }
+}
